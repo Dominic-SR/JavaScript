6425b16ebb377f20d30a31f2bdcec2c24ab8a196
Dominic-SR <dominicseduraja3@gmail.com>
2025-05-04 22:38:45 +0530

    template litral added

--- JS-Interview-Therory-Questions/Top-30-JS-Questions.js
+++ JS-Interview-Therory-Questions/Top-30-JS-Questions.js
@@ -6,4 +6,14 @@ JavaScript is a highlevel programming language that is widely used to create int
 effects within web browsers a highlevel programming language is a human understandable language it is commonly
 used for developing both web and mobile applications
 
+2. what are templateliterals in JavaScript ?
+
+template literals are what you get when you combine the dollar sign curly brackets and the back
+ticks now the reason why we need them is to Simply make our coding lives easier let me tell you how if we have two
+variables a with the value 10 and B with the value 20 and we want to print their
+sum on the console it would look something like this this code is now not so clean and requires a lot of work
+hence we use template literals they neatly enclose everything within back ticks with executable code placed inside
+the dollar signs and curly braces like this so in a nutshell template literals are used to embed variables and
+expressions directly into a string making the code more readable and flexible
+
 */
