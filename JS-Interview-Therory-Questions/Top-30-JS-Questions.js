6fa2408357ab454ab5609685628b1d91f06e67a5
Dominic-SR <dominicseduraja3@gmail.com>
2025-05-04 22:36:53 +0530

    js questions added

---
+++ JS-Interview-Therory-Questions/Top-30-JS-Questions.js
@@ -0,0 +1,9 @@
+/* 
+
+1. What is java script and where is it commonly used ?
+
+JavaScript is a highlevel programming language that is widely used to create interactive
+effects within web browsers a highlevel programming language is a human understandable language it is commonly
+used for developing both web and mobile applications
+
+*/
